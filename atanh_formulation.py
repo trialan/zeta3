3471e6d958e2f587e020fe6c0335695ad77eaf1f
Thomas Rialan <thomasrialan@gmail.com>
2023-10-22 22:41:05 +0200

    exploration

--- atanh_formulation.py
+++ atanh_formulation.py
@@ -1,26 +1,33 @@
 import sympy as sp
 
-# Symbolic variable declaration
-n, k, N = sp.symbols('n k N')
-a_n = sp.atanh(1/n**3)
+n, k = sp.symbols('n k')
+a_n = sp.Symbol('a_n')
 
-# Initial value for T_2
-T_2 = sp.tanh(a_n.subs(n, 2))
 
-# Recursive formula for T_k
-T_k = sp.Function('T')(k)
-T_k_minus_1 = sp.Function('T')(k-1)
-tanh_sum_formula = (T_k_minus_1 + sp.tanh(a_n.subs(n, k))) / (1 + T_k_minus_1 * sp.tanh(a_n.subs(n, k)))
+def tanh_add_formula(a, b):
+    return (a + b) / (1 + a * b)
 
-# Expressing T_k in terms of T_{k-1}
-T_k_expr = sp.Eq(T_k, tanh_sum_formula)
 
-# Generating the expressions for several terms
-results = [T_2]
-for i in range(3, N+1):
-    term = T_k_expr.subs(k, i)
-    results.append(term)
+def construct_recursive_formula(k, previous_formula, a_n_seq):
+    """
+    Parameters:
+    - k: The current index for which the formula is to be generated.
+    - previous_formula: The formula for T_{k-1}.
+    - a_n_seq: The symbolic representation of the sequence a_n.
+    """
+    return tanh_add_formula(previous_formula, sp.tanh(a_n_seq.subs(n, k)))
 
-for result in results:
-    print(result)
 
+def generate_formulas(N, a_n_seq):
+    formulas = {2: sp.tanh(a_n_seq.subs(n, 2))}
+    for i in range(3, N+1):
+        formula = formulas[i-1]
+        for j in range(i-1, 2, -1):
+            formula = formula.subs(formulas[j], construct_recursive_formula(j, formulas[j-1], a_n_seq))
+        formulas[i] = construct_recursive_formula(i, formula, a_n_seq)
+    return formulas
+
+
+if __name__ == '__main__':
+    example_formulas = generate_formulas(10, a_n)
+    print(example_formulas)
