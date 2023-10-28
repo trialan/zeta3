81dfa9a1c4234706bbe32076823e93b8c318dd6a
Thomas Rialan <thomasrialan@gmail.com>
2023-10-28 19:14:37 +0200

    correct formula

--- atanh_formulation.py
+++ atanh_formulation.py
@@ -1,35 +1,20 @@
+from pprint import pprint
 import sympy as sp
 from zeta3.latex import generate_pdf
 
-n, k = sp.symbols('n k')
-a_n = sp.Symbol('a_n')
-
 
 def tanh_add_formula(a, b):
-    return (a + b) / (1 + a * b)
+    return (sp.tanh(a) + sp.tanh(b)) / (1 + sp.tanh(a) * sp.tanh(b))
 
 
-def construct_recursive_formula(k, previous_formula, a_n_seq):
-    """
-    Parameters:
-    - k: The current index for which the formula is to be generated.
-    - previous_formula: The formula for T_{k-1}.
-    - a_n_seq: The symbolic representation of the sequence a_n.
-    """
-    return tanh_add_formula(previous_formula, sp.tanh(a_n_seq.subs(n, k)))
-
-
-def generate_formulas(N, a_n_seq):
-    formulas = {2: sp.tanh(a_n_seq.subs(n, 2))}
-    for i in range(3, N+1):
-        formula = formulas[i-1]
-        for j in range(i-1, 2, -1):
-            formula = formula.subs(formulas[j], construct_recursive_formula(j, formulas[j-1], a_n_seq))
-        formulas[i] = construct_recursive_formula(i, formula, a_n_seq)
+def generate_specific_formulas(N):
+    a_terms = [sp.Symbol(f'a_{i}') for i in range(0, N)]
+    formulas = {0: sp.tanh(a_terms[0])}
+    for i in range(1, N):
+        formulas[i] = (formulas[i-1] + sp.tanh(a_terms[i])) / (1 + formulas[i-1] * sp.tanh(a_terms[i]))
     return formulas
 
 
-
 if __name__ == '__main__':
-    example_formulas = generate_formulas(10, a_n)
-    generate_pdf(example_formulas)
+    example_formulas = generate_specific_formulas(5)
+    pprint(example_formulas)
