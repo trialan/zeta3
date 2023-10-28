a8f4f25e0ab776e245f15fa53ce97c7c06ceae13
Thomas Rialan <thomasrialan@gmail.com>
2023-10-28 20:30:10 +0200

    generate PDF of formulae

--- latex.py
+++ latex.py
@@ -1,33 +1,65 @@
-from sympy.printing import latex
+import sympy as sp
+import subprocess
 import os
-import shutil
-import tempfile
 
 
-def generate_pdf(formulas, output_dir="."):
-    latex_content = generate_latex_content(formulas)
-    generate_pdf_from_latex(latex_content, output_dir)
+def generate_list_latex_document(formulas):
+    document = """
+\\documentclass{article}
+\\usepackage{graphicx} % Required for inserting images
+\\usepackage[landscape]{geometry} % For landscape orientation
+\\usepackage{amsmath} % For mathematical formatting
 
+\\title{Atanh - Zeta3}
+\\author{thomas.rialan}
+\\date{October 2023}
 
-def generate_latex_content(formulas):
-    """Generate LaTeX content based on the provided formulas."""
-    latex_content = r"""
-\documentclass{article}
-\usepackage{amsmath}
-\begin{document}
+\\begin{document}
+
+\\maketitle
+
+\\section{Introduction}
 """
-    for k, formula in formulas.items():
-        latex_formula = latex(formula)
-        latex_content += r"\[ T_{" + str(k) + "} = " + latex_formula + r" \]" + "\n"
-    latex_content += r"\end{document}"
-    return latex_content
+    for i, formula in enumerate(formulas):
+        numerator_terms = formula.as_numer_denom()[0].as_ordered_terms()
+        denominator_terms = formula.as_numer_denom()[1].as_ordered_terms()
+        numerator_latex = ', '.join([sp.latex(term) for term in numerator_terms])
+        denominator_latex = ', '.join([sp.latex(term) for term in denominator_terms])
+        document += f"\nNumerator terms: \\[{numerator_latex}\\]\n"
+        document += f"Denominator terms: \\[{denominator_latex}\\]\n"
+    document += "\n\\end{document}"
+    return latex_to_pdf(document)
 
 
-def generate_pdf_from_latex(latex_content, output_path):
-    temp_dir = tempfile.mkdtemp()
-    latex_file_path = os.path.join(temp_dir, "output.tex")
-    with open(latex_file_path, "w") as latex_file:
-        latex_file.write(latex_content)
-    os.system(f"pdflatex -output-directory={temp_dir} {latex_file_path}")
-    shutil.move(os.path.join(temp_dir, "output.pdf"), output_path)
-    shutil.rmtree(temp_dir)
+def generate_latex_document(formulae):
+    document = """
+\\documentclass{article}
+\\usepackage{graphicx} % Required for inserting images
+\\usepackage[landscape]{geometry} % For landscape orientation
+\\usepackage{breqn} % For automatic equation breaking
+
+\\title{zeta(3) approximation}
+\\author{thomas.rialan}
+\\date{October 2023}
+
+\\begin{document}
+
+\\maketitle
+
+\\section{Introduction}
+"""
+    for i, formula in formulae.items():
+        formula_latex = sp.latex(formula)
+        document += f"\n\\begin{{dmath*}}\n{formula_latex}\n\\end{{dmath*}}\n"
+    document += "\n\\end{document}"
+    return latex_to_pdf(document)
+
+
+def latex_to_pdf(latex_content, output_filename="output_latex/output"):
+    with open(output_filename + ".tex", 'w') as file:
+        file.write(latex_content)
+    subprocess.run(["pdflatex", output_filename + ".tex"])
+    if os.path.exists(output_filename + ".pdf"):
+        print(f"PDF generated: {output_filename}.pdf")
+    else:
+        print("Error generating PDF.")
