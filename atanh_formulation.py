763ad0b3e8c74c06af748271ae1d0acab1b751e5
Thomas Rialan <thomasrialan@gmail.com>
2023-10-28 20:30:27 +0200

    refactor

--- atanh_formulation.py
+++ atanh_formulation.py
@@ -1,20 +1,47 @@
 from pprint import pprint
 import sympy as sp
-from zeta3.latex import generate_pdf
+from zeta3.latex import generate_latex_document, generate_list_latex_document
 
 
-def tanh_add_formula(a, b):
-    return (sp.tanh(a) + sp.tanh(b)) / (1 + sp.tanh(a) * sp.tanh(b))
-
-
-def generate_specific_formulas(N):
+def generate_partial_formulae(N):
     a_terms = [sp.Symbol(f'a_{i}') for i in range(0, N)]
-    formulas = {0: sp.tanh(a_terms[0])}
+    formulae = [sp.tanh(a_terms[0])]
     for i in range(1, N):
-        formulas[i] = (formulas[i-1] + sp.tanh(a_terms[i])) / (1 + formulas[i-1] * sp.tanh(a_terms[i]))
-    return formulas
+        formulae.append(generate_f_recursively(formulae[i-1], a_terms[i]))
+        formulae[i] = simplify(formulae[i])
+    return formulae[1:]
+
+
+def generate_f_recursively(last_f, next_term):
+    out = (last_f + sp.tanh(next_term)) / (1 + last_f * sp.tanh(next_term))
+    return out
+
+
+def lambdify_formula(formula):
+    """Convert a symbolic formula to a numerical function."""
+    symbols = sorted(formula.free_symbols, key=lambda x: x.name)
+    numerical_function = sp.lambdify(symbols, formula, 'numpy')
+    return numerical_function
+
+
+def simplify(formula):
+    expanded_formula = expand(formula)
+    simplified_formula = sp.simplify(expanded_formula)
+    return simplified_formula
+
+
+def expand(formula):
+    expanded_formula = sp.expand(formula)
+    for symbol in expanded_formula.free_symbols:
+        if "tanh" in str(symbol):
+            expanded_formula = expanded_formula.subs(symbol,
+                                                     sp.atanh(sp.tanh(symbol)))
+    return expanded_formula
 
 
 if __name__ == '__main__':
-    example_formulas = generate_specific_formulas(5)
-    pprint(example_formulas)
+    formulae = generate_partial_formulae(5)
+    doc = generate_list_latex_document(formulae)
+
+
+
